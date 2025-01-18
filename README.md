6e6fa575a94abd6572a6909599005a507ee2b4e6
Jimmxyz <Jimmynoux@protonmail.com>
2025-01-18 12:13:08 +0100

    First part
    Warning : Bug : Winning Condition fail

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyConnect4-TI
 A Connect 4 for Python Calculator (or CLI)
+
+## License
+This Program is under MIT License 
+This means that anyone can use this code as they wish.
---
+++ Script/script.py
@@ -0,0 +1,137 @@
+try:
+    import termcolor as tc
+    from termcolor import colored
+    print("termcolor library imported sucessfuly")
+    color_cli = True
+except ImportError:
+    print("termcolor failed to be imported")
+    color_cli = False
+turn = "X"
+move = 0
+grid = [
+    ["0","1","2","3","4","5","6","7","8","9"],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "],
+    [" "," "," "," "," "," "," "," "," "," "]
+]
+
+def header():
+    if color_cli == True:
+        if turn == "X":
+            print("[Connect 4] Move "+ str(move) + " ("+colored("X","red")+" turn)")
+        elif turn == "O":
+            print("[Connect 4] Move "+ str(move) + " ("+colored("O","blue")+" turn)")
+    else:
+        print("[Connect 4] Move "+ str(move) + " ("+turn+" turn)")
+
+def gridPrint():
+    for i in range(9):
+        for j in range(10):
+            if color_cli == True:
+                if grid[i][j] == "X":
+                    print("["+colored("X","red"), end="]")
+                elif grid[i][j] == "O":
+                    print("["+colored("O","blue"), end="]")
+                elif grid[i][j] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                    print("["+colored(grid[i][j],"grey"), end="]")
+                elif grid[i][j] == "!4":
+                    print("["+colored("4","yellow"), end="]")
+                else:
+                    print("["+str(grid[i][j]), end="]")
+            else:
+                print("["+str(grid[i][j]), end="]")
+        print()
+
+def gameTurn():
+    global turn, move
+    header()
+    gridPrint()
+    if move == 0:
+        print("Type the number of a row")
+    moveID = input()
+    if moveID not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        if len(moveID) > 3:
+            print(str(moveID[:3] + "... is not a valid number"))
+            input()
+            return gameTurn()
+        else:
+            print(str(moveID + " is not a valid number"))
+            return gameTurn()
+    else:
+        emptySpaceFinder(int(moveID))
+        winCheck()
+        if turn == "X":
+            turn = "O"
+        elif turn == "O":
+            turn = "X"
+        return gameTurn()
+
+def emptySpaceFinder(row):
+    if grid[0][row] == "X" or grid[0][row] == "O":
+        print("The row " + row + " is full")
+    else:
+        for i in range(2,9):
+            if grid[i][row] == "X" or grid[i][row] == "O":
+                grid[i-1][row] = turn
+                break
+        grid[8][row] = turn
+    return
+
+def winCheck():
+    #Line check
+    x_streak = 0
+    o_streak = 0
+    for i in range(9):
+        x_streak = 0
+        o_streak = 0
+        for j in range(10):
+            if grid[i][j] == "X":
+                x_streak += 1
+                o_streak = 0
+            elif grid[i][j] == "O":
+                x_streak = 0
+                o_streak += 1
+            else:
+                x_streak = 0
+                o_streak = 0
+            if x_streak == 4:
+                win("X",i,i,j-4,j)
+                break
+            elif o_streak == 4:
+                win("O",i,i,j-4,j)
+                break
+
+def win(winner,lineStart,lineEnd,rowStart,rowEnd):
+    grid[lineStart][rowStart] = "!4"
+    grid[lineEnd][rowEnd] = "!4"
+    lineSec = 0
+    lineTrd = 0
+    rowSec = 0
+    rowTrd = 0
+    if lineStart == lineEnd:
+        lineSec = lineStart
+        lineTrd = lineStart
+    elif lineStart > lineEnd:
+        lineSec = lineStart - 1
+        lineTrd = lineStart - 2
+    elif lineStart < lineEnd:
+        lineSec = lineStart + 1
+        lineTrd = lineStart + 2
+    if rowStart == rowEnd:
+        rowSec = rowStart
+        rowTrd = rowStart
+    elif rowStart > rowEnd:
+        rowSec = rowStart - 1
+        rowTrd = rowStart - 2
+    elif rowStart < rowEnd:
+        rowSec = rowStart + 1
+        rowTrd = rowStart + 2
+    grid[lineSec][rowSec] = "!4"
+    grid[lineTrd][rowTrd] = "!4"
+
+gameTurn()
